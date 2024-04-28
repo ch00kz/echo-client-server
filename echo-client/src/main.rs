ec56bf0f8cb8dc53b999b86ff7b7b921ed41b559
Andrew Thomas <andrewt@mercury.com>
2024-04-27 20:39:41 -0400

    only read the num of bytes we received from our buffer

--- echo-client/src/main.rs
+++ echo-client/src/main.rs
@@ -14,17 +14,17 @@ fn main() {
     // writing message
     let message = "Hello World!";
     let terminal_char = "\n";
-    let sent_bytes = stream
+    let num_bytes_sent = stream
         .write(format!("{message}{terminal_char}").as_bytes())
         .unwrap();
     stream.flush().unwrap();
-    println!("[Sent] (Bytes: {sent_bytes}): {}", message);
+    println!("[Sent] ({num_bytes_sent} bytes): {}", message);
 
     // reading message
     let mut read_buffer: [u8; 1024] = [0; 1024];
-    let read_bytes = stream.read(&mut read_buffer).unwrap();
+    let num_bytes_read = stream.read(&mut read_buffer).unwrap();
     println!(
-        "[Received] (Bytes: {read_bytes}): {}",
-        String::from_utf8_lossy(&read_buffer)
+        "[Received] ({num_bytes_read} bytes): {}",
+        String::from_utf8_lossy(&read_buffer[..num_bytes_read])
     );
 }
